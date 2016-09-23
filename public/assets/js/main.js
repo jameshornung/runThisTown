9353064267297c609616ada9eabfecad6cd66a33
James Hornung <hornung.james@gmail.com>
2016-09-22 21:41:27 -0500

    fixed dropdown

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
 	$(".button-collapse").sideNav();
 	$('select').material_select();
 	
-	// $('.butlerDropdowns').click(function(){
+	$('.butlerDropdowns').click(function(){
 
-	// 	var x = this.value
-	// 	console.log(x);
-	// 	$('#routeImage').attr("src", butlerLoop[x].image);
-	// 	$('#routeTitle').text(butlerLoop[x].title);
-	// 	$('#routeDescription').html(butlerLoop[x].description);
-	// 	$('#routeButton').attr("href", butlerLoop[x].routeHref);
-	// })
+		var x = this.value
+		console.log(x);
+		$('#routeImage').attr("src", butlerLoop[x].image);
+		$('#routeTitle').text(butlerLoop[x].title);
+		$('#routeDescription').html(butlerLoop[x].description);
+		$('#routeButton').attr("href", butlerLoop[x].routeHref);
+	})
 })
