a15b44623334ab28a8295780eea25415d73de63d
James Hornung <hornung.james@gmail.com>
2016-10-03 17:23:29 -0500

    complete half marathon training plan entered

--- public/assets/js/training.js
+++ public/assets/js/training.js
@@ -4,11 +4,11 @@ var halfIntermediate = [
 	{week: ["40 Min Cross", "3.5 Mile Run", "5 Mile Run", "3.5 Mile Run", "Rest", "Rest","6 Mile Run"]},
 	{week: ["40 Min Cross", "3.5 Mile Run", "5 Mile Pace", "3.5 Mile Run", "Rest", "3 Mile Run","7 Mile Run"]},
 	{week: ["40 Min Cross",	"4 Mile Run", "6 Mile Run",	"4 Mile Run", "Rest", "3 Mile Pace", "8 Mile Run"]},
-	{week: ["50 Min Cross", "4 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "4.5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["50 Min Cross", "4.5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["60 Min Cross", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["60 Min Cross", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "4 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]}
+	{week: ["50 Min cross", "4 Mile Run", "6 Mile Pace", "4 Mile Run", "Rest or Easy Run", "Rest", "5-K Race"]},
+	{week: ["Rest", "4.5 Mile Run", "7 Mile Run", "4.5 Mile Run", "Rest", "4 Mile Pace", "9 Mile Run"]},
+	{week: ["50 Min cross", "4.5 Mile Run", "7 Mile Pace", "4.5 Mile Run", "Rest", "5 Mile Pace", "10 Mile Run"]},
+	{week: ["60 Min cross", "5 Mile Run", "8 Mile Run", "5 Mile Run", "Rest or Easy Run", "Rest", "10-K Race"]},
+	{week: ["Rest", "5 Mile Run", "8 Mile Pace", "5 Mile Run", "Rest", "5 Mile Pace", "11 Mile Run"]},
+	{week: ["60 Min cross", "5 Mile Run", "6 Mile Run", "4 Mile Run", "Rest", "3 Mile Pace", "12 Mile Run"]},
+	{week: ["Rest", "4 Mile Run", "4 Mile Pace", "2 Mile Run", "Rest", "Rest", "Half Marathon"]}
 ];
