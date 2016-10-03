e49f13a6967c22b8dae8c2a1214b93b30eec9cf3
James Hornung <hornung.james@gmail.com>
2016-10-03 16:26:05 -0500

    halfway done logging the half marathon training plan

--- public/assets/js/training.js
+++ public/assets/js/training.js
@@ -1,14 +1,14 @@
 var halfIntermediate = [
 	{week: ["30 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["30 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["40 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["40 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["40 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["50 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["50 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["60 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["60 Min Cross", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
-	{week: ["Rest", "3 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]}
+	{week: ["30 Min Cross", "3 Mile Run", "4 Mile Pace", "3 Mile Run", "Rest", "3 Mile Pace","5 Mile Run"]},
+	{week: ["40 Min Cross", "3.5 Mile Run", "5 Mile Run", "3.5 Mile Run", "Rest", "Rest","6 Mile Run"]},
+	{week: ["40 Min Cross", "3.5 Mile Run", "5 Mile Pace", "3.5 Mile Run", "Rest", "3 Mile Run","7 Mile Run"]},
+	{week: ["40 Min Cross",	"4 Mile Run", "6 Mile Run",	"4 Mile Run", "Rest", "3 Mile Pace", "8 Mile Run"]},
+	{week: ["50 Min Cross", "4 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["Rest", "4.5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["50 Min Cross", "4.5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["60 Min Cross", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["Rest", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["60 Min Cross", "5 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]},
+	{week: ["Rest", "4 Mile Run", "4 Mile Run", "3 Mile Run", "Rest", "3 Mile Run","4 Mile Run"]}
 ];
