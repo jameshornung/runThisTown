03bda633551434502032cd334d404b57cc5c4909
James Hornung <hornung.james@gmail.com>
2016-10-05 19:02:41 -0500

    wrote some route descriptions

--- public/assets/js/routes.js
+++ public/assets/js/routes.js
@@ -2,30 +2,30 @@ var butlerLoop = [
 {
 	image: '/assets/images/routes/morningBoardwalk.jpg',
 	title: 'The 10 Mile Loop',
-	description: "The Man Inside Me seems well reviewed. First I blow him, then I poke him. Can you believe that the only reason the club is going under is because it's in a terrifying neighborhood? Oh, like when they say 'poofter' to mean 'tourist', yes. I was hoping he would be gifted sexually. I'm gonna build me an airport, put my name on it. Why, Michael? So you can fly away from your feelings?  How am I supposed to find someone willing to go into that musty old claptrap? It just seems like there's still light coming in from under the door. You go buy a tape recorder and record yourself for a whole day. I think you'll be surprised at some of your phrasing. It looks like you've been looking for dragons… in the future. Even it means me taking a chubby… I will suck it up."
+	description: "<p>" + "The Butler Trail is a 10 mile hike and bike trail that meanders along the bank of Ladybird Lake in downtown Austin and is considered the crown jewel of our running routes. Attracting more than 1.5 million visitors a year the trail is Austin's most popular recreational area. In this city we pride ourselves on being weird and being active, and both attributes are on full display along the trail. On a given run you are likely to see unicyclist, slack rope walkers, paddle boarders, dance walkers, and acro yogis. One of the great things about the Butler Trail is you never know what you will run across and there is always something new to discover." + "</p><p>" + "The full trail is a 10 mile loop that follows Ladybird Lake between the Mopac Bridge and Longhorn Dam passing by many of Austin’s iconic landmarks along the way, but one of the best features of the trail is that the numerous bridges allow for a loop of almost any distance, and the multiple trailheads make the trail easily accessible from anywhere in the downtown area." + "</p>"
 },{
 	image: '/assets/images/routes/eastside.jpg',
 	title: 'I-35 to Longhorn Dam',
-	description: 'Describe this route'
+	description: "<p>" + "This east side loop of the trail is traditionally underutilized, although with the completion of the boardwalk project in 2014 and the revitalization of Riverside Drive there has been a significant influx of runners in recent years.  Even with the increase in usage it is still a great place to head to avoid the congestion that sometimes builds in other parts of the trail.  This route offers some of the newest additions to the trail system as well as some relics from Austin’s past.  The new Lakeshore Point Trailhead is a fantastic spot to start a run providing: ample parking along Lakeshore Drive; bathrooms; bike racks; and an outdoor workout station." + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "Along this route you will find some of the most unique urban artwork available at the trail.  You won’t want to miss the Selena memorial painted on the base of the transmission tower along Pleasant Valley, or the graffiti art and murals  decorating the walls of the old Holly Street Power Plant.  In addition to the street art, the city has also included a series of statues known as the Trail of Tejano Legends commemorating the significant musical contributions of Austin’s Tejano and Hispanic communities to the live music capital of the world." + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "The Draft Pick, located in the the Amli Apartments on Lakeshore Drive, has outdoor seating and is a great place to grab a post run beer.  On a hot day, snow cones from the food trailers next door can also be a welcome treat!" + "</p>" 
 },{
 	image: '/assets/images/routes/boardwalk.jpg',
 	title: 'I-35 to Congress',
-	description: 'Describe this route'
+	description: "<p>" + "This loop of the trail includes stunning views of the Austin skyline, the main section of the boardwalk, and an up close look at Austin’s famous bat bridge, home to the world’s largest urban bat colony." + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "" + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "Dominican Jo’s coffeeshop, located at Riverside and Congress, is a great place to meet for coffee before or after a run.  The Thom’s Market across the street also offers a wide variety of rehydrating beverages and a full array of sandwiches and snacks." + "</p>"
 },{
 	image: '/assets/images/routes/skyline.jpg',
 	title: 'Mopac to Pfluger Bridge',
-	description: 'Describe this route'
+	description: "<p>" + "This is the most populated area of the trail, and provides some of the best opportunities for people watching.  A significant stretch of the route runs along the border of Zilker park and depending on the season provides unique running experiences.  In September you can hear the bands of the Austin City Limit musical festival, in December you can enjoy the Christmas spirit encapsulated at the trail of lights, and all year the vibrant spirit of the park and the city permeates this loop.  The Pfluger Bridge and Lou Neff Point provide picturesque views of downtown Austin." + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "Completion of the Pfuger Bridge in 2001 greatly increased the safety of the trail by getting runners and bikers away from the heavily trafficked Lamar Avenue.  There are numerous benches lining the pedestrian bridge and it is great place to sit and enjoy the views." + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "Luke’s Locker is located just off the North side of the Pfluger bridge and is easily accessible from the trail.  They always have an igloo full of ice cold water and a store full of anything your running heart might desire.  Ready to call it a day?  Whole Foods is a quarter mile up Lamar and has a rooftop patio that is the ideal place to open a post run bottle of wine." + "</p>"
 },{
-	image: '/assets/images/routes/fullLoop.jpeg',
+	image: '/assets/images/routes/',
 	title: 'Mopac to 1st',
-	description: 'Describe this route'
+	description: "<p>" + "" + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "" + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "" + "</p>"
 },{
-	image: '/assets/images/routes/fullLoop.jpeg',
+	image: '/assets/images/routes/',
 	title: 'Mopac to Congress',
-	description: 'Describe this route'
+	description: "<p>" + "" + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "" + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "" + "</p>"
 },{
-	image: '/assets/images/routes/fullLoop.jpeg',
+	image: '/assets/images/routes/',
 	title: 'Mopac to I-35',
-	description: 'Describe this route'
+	description: "<p>" + "" + "</p><p><strong>" + "Notable Route Features: " + "</strong>" + "" + "</p><p><strong>" + "Insider Tips: " + "</strong>" + "" + "</p>"
 }
 ];
